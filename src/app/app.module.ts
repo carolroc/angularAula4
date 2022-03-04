e5eca9d906ffc09c5ff7e4fab5d7aaa80d25b4e6
carolroc <carolroc02@gmail.com>
2022-03-04 19:51:43 -0300

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component'; //vem quando importa o home component lá em baixo
+import { RouterModule } from '@angular/router'; //vem do que colocamos la em baixo
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { NumbersComponent } from './numbers/numbers.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent }, //passa os parametros caminho e o componente
+      { path: 'about', component: AboutComponent },
+      { path: 'numbers', component: NumbersComponent },
+    ]), //configura as rotas a partir da raiz
+  ],
+  declarations: [AppComponent, HelloComponent],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
+
+// fica todas as declarações que vamos usar na pasta app
